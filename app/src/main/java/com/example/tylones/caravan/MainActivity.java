81f9d42a0c11cdb4a0795c4666492c44ea20551a
Jérémie Levet <jeremie.levet@gmail.com>
2017-11-25 23:36:06 +0100

    test WC

--- app/src/main/java/com/example/tylones/caravan/MainActivity.java
+++ app/src/main/java/com/example/tylones/caravan/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.tylones.caravan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.HttpURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton btnWC = (ImageButton)findViewById(R.id.btnWC);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) throws IOException {
+        if(v == btnWC){
+            URL url = new URL("172.20.10.3/WC");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            } finally {
+                urlConnection.disconnect();
+            }
+        }
     }
 }
